1d892a8bd0b3b31060eea24a66a148942047aa4c
phapnm18 <95583997+phapnm18@users.noreply.github.com>
2022-05-27 13:38:32 +0700

    Update ErrMsgMngtDBDAO.java

--- practice 1/errmsgmngt/integration/ErrMsgMngtDBDAO.java
+++ practice 1/errmsgmngt/integration/ErrMsgMngtDBDAO.java
@@ -44,41 +44,49 @@ public class ErrMsgMngtDBDAO extends DBDAOSupport {
 	 @SuppressWarnings("unchecked")
 	 //	 declare function searchErrMsgVO
 	public List<ErrMsgVO> searchErrMsgVO(ErrMsgVO errMsgVO) throws DAOException {
-		//	assign the default value of null to 2 variables
+		// assign the default value of null to 2 variables
 		DBRowSet dbRowset = null;
 		List<ErrMsgVO> list = null;
-		//query parameter
+		// query parameter
 		Map<String, Object> param = new HashMap<String, Object>();
-		//velocity parameter
+		// velocity parameter
 		Map<String, Object> velParam = new HashMap<String, Object>();
-		//	exception handling for arrays	
+		// exception handling for arrays	
 		try{
-			//	check errMsgVO is empty or not
+			
 			if(errMsgVO != null){
-				//	assign a value to mapVO 
+				// assign a value to mapVO 
 				Map<String, String> mapVO = errMsgVO .getColumnValues();
-				//	put mapVO in to param & velparam
+				// put values in mapVO into param & velparam
 				param.putAll(mapVO);
 				velParam.putAll(mapVO);
 			}  
-			//	execute SQL queries to read the data	
+			// execute queries in ErrMsgMgmtDBDAOErrMsgVORSQL with param and velParam	
 			dbRowset = new SQLExecuter("").executeQuery((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgVORSQL(), param, velParam);
 			list = (List)RowSetUtil.rowSetToVOs(dbRowset, ErrMsgVO .class);
-			//	Coins for JDBC			
+					
 		} catch(SQLException se) {
-			//	throw console.log a message error 
+			// throw console.log a message error 
 			log.error(se.getMessage(),se);
+			// throw EventException
 			throw new DAOException(new ErrorHandler(se).getMessage());
-			//	handling errors for mapvo
+			// handling errors for mapvo
 		} catch(Exception ex) {
-			//	throw console.log a message error
+			// throw console.log a message error
 			log.error(ex.getMessage(),ex);
+			// throw EventException
 			throw new DAOException(new ErrorHandler(ex).getMessage());
 		}
-		//	returns a list of elements
+
 		return list;
 	}
-	 //	declare function addManageErrMsgVO
+	 /**
+	 * [addmanageErrMsgVO] to add ErrMsgVO.<br>
+	 * 
+	 * @param ErrMsgVO errMsgVO
+	 * @exception DAOException
+	 * @exception Exception
+	 */
 	public void addManageErrMsgVO(ErrMsgVO errMsgVO) throws DAOException,Exception {
 		//query parameter
 		Map<String, Object> param = new HashMap<String, Object>();
@@ -86,103 +94,126 @@ public class ErrMsgMngtDBDAO extends DBDAOSupport {
 		Map<String, Object> velParam = new HashMap<String, Object>();
 		//	exception handling for arrays
 		try {
-			//	assign a value to mapVO
+			// assign a value to mapVO
 			Map<String, String> mapVO = errMsgVO .getColumnValues();
-			//	put mapVO into param & velparam
+			// put values in mapVO into param & velparam
 			param.putAll(mapVO);
 			velParam.putAll(mapVO);
-			//	create new SQLExecute
+			// create new SQLExecute
 			SQLExecuter sqlExe = new SQLExecuter("");
-			//	create SQL queries to update data
+			// execute queries in ErrMsgMngtDBDAOErrMsgVOCSQL with param and velParam
 			int result = sqlExe.executeUpdate((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgVOCSQL(), param, velParam);
-			//	throw an DAOException to know about connection to SQL			
+			// EXECUTE_FAILED: The constant indicating that an error occured while executing a batch statement.
+			// if result = execute_failed throw error			
 			if(result == Statement.EXECUTE_FAILED)
 					throw new DAOException("Fail to insert SQL");
-			//	Coins for JDBC 
+			
 		} catch(SQLException se) {
-			//	throw console.log a message error
+			// throw console.log a message error
 			log.error(se.getMessage(),se);
+			// throw EventException
 			throw new DAOException(new ErrorHandler(se).getMessage());
-			//	handling errors for mapvo
+			// handling errors for mapvo
 		} catch(Exception ex) {
-			//	throw console.log a message error
+			// throw console.log a message error
 			log.error(ex.getMessage(),ex);
+			// throw EventException
 			throw new DAOException(new ErrorHandler(ex).getMessage());
 		}
 	}
 	
 	 //	declare function modifyManageErrMsgVO
 	public int modifyManageErrMsgVO(ErrMsgVO errMsgVO) throws DAOException,Exception {
-		//query parameter
+		// query parameter
 		Map<String, Object> param = new HashMap<String, Object>();
-		//velocity parameter
+		// velocity parameter
 		Map<String, Object> velParam = new HashMap<String, Object>();
-		//	declare the value result
+		// declare the value result
 		int result = 0;
-		//	exception handling for arrays
+		// exception handling for arrays
 		try {
-			//	assign a value to mapVO
+			// assign a value to mapVO
 			Map<String, String> mapVO = errMsgVO .getColumnValues();
-			//	put mapVO into param & velparam
+			// put values in mapVO into param & velparam
 			param.putAll(mapVO);
 			velParam.putAll(mapVO);
-			//	create new SQLExecute
+			// create new SQLExecute
 			SQLExecuter sqlExe = new SQLExecuter("");
-			//	create SQL queries to update data
+			// execute queries in ErrMsgMngtDBDAOErrMsgVOUSQL with param and velParam
 			result = sqlExe.executeUpdate((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgVOUSQL(), param, velParam);
-			//	throw an DAOException to know about connection to SQL
+			// EXECUTE_FAILED: The constant indicating that an error occured while executing a batch statement.
+			// if result = execute_failed throw error
 			if(result == Statement.EXECUTE_FAILED)
 					throw new DAOException("Fail to insert SQL");
-			//	Coins for JDBC
+		
 		} catch(SQLException se) {
-			//	throw console.log a message error
+			// throw console.log a message error
 			log.error(se.getMessage(),se);
+			// throw EventException
 			throw new DAOException(new ErrorHandler(se).getMessage());
-			//	handling errors for mapvo
+			// handling errors for mapvo
 		} catch(Exception ex) {
-			//	throw console.log a message error
+			// throw console.log a message error
 			log.error(ex.getMessage(),ex);
+			// throw EventException
 			throw new DAOException(new ErrorHandler(ex).getMessage());
 		}
-		//	return results after processing is done
+		// return results after processing is done
 		return result;
 	}
 	
 
-	// declare function addManageErrMsgVOS
+	/**
+	 * [addmanageErrMsgVOS] to add ErrMsgVOs.<br>
+	 * 
+	 * @param List<ErrMsgVO> errMsgVO
+	 * @return int[]
+	 * @exception DAOException
+	 * @exception Exception
+	 */
 	public int[] addManageErrMsgVOS(List<ErrMsgVO> errMsgVO) throws DAOException,Exception {
-		//	declare the array insCnt
+		// declare the array insCnt
 		int insCnt[] = null;
-		//	exception handling for arrays
+		
 		try {
-			//	create new SQLExecute
+			// create new SQLExecuter variable to execute query
 			SQLExecuter sqlExe = new SQLExecuter("");
-			//	check the size of the array
+			// check if list not null, execute query to save add new changes in database
 			if(errMsgVO .size() > 0){
-				//	create SQL queries to insert data
+				// execute queries in ErrMsgMngtDBDAOErrMsgVOCSQL with param and velParam
 				insCnt = sqlExe.executeBatch((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgVOCSQL(), errMsgVO,null);
 				//	iterate the elements in the array		
 				for(int i = 0; i < insCnt.length; i++){
-					//	throw an DAOException to know about connection to SQL
+					// EXECUTE_FAILED: The constant indicating that an error occured while executing a batch statement.
+					// if result = execute_failed throw error
 					if(insCnt[i]== Statement.EXECUTE_FAILED)
 						throw new DAOException("Fail to insert No"+ i + " SQL");
 				}
 			}
-			//	Coins for JDBC
+			
 		} catch(SQLException se) {
 			//	throw console.log a message error
 			log.error(se.getMessage(),se);
+			//	throw EventException
 			throw new DAOException(new ErrorHandler(se).getMessage());
 			//	handling errors
 		} catch(Exception ex) {
 			//	throw console.log a message error
 			log.error(ex.getMessage(),ex);
+			//	throw EventException
 			throw new DAOException(new ErrorHandler(ex).getMessage());
 		}
 		//	return array
 		return insCnt;
 	}
-	// declare function modifyManageErrMsgVOS
+	/**
+	 * [modifymanageErrMsgVOS] to update ErrMsgVOs.<br>
+	 * 
+	 * @param List<ErrMsgVO> errMsgVO
+	 * @return int[]
+	 * @exception DAOException
+	 * @exception Exception
+	 */
 	public int[] modifyManageErrMsgVOS(List<ErrMsgVO> errMsgVO) throws DAOException,Exception {
 		//	declare the array updCnt
 		int updCnt[] = null;
@@ -192,62 +223,118 @@ public class ErrMsgMngtDBDAO extends DBDAOSupport {
 			SQLExecuter sqlExe = new SQLExecuter("");
 			//	check the size of the array
 			if(errMsgVO .size() > 0){
-				//	create SQL queries to update data
+				// execute queries in ErrMsgMngtDBDAOErrMsgVOUSQL with param and velParam
 				updCnt = sqlExe.executeBatch((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgVOUSQL(), errMsgVO,null);
 				//	iterate the elements in the array
 				for(int i = 0; i < updCnt.length; i++){
-					//	throw an DAOException to know about connection to SQL
+					// EXECUTE_FAILED: The constant indicating that an error occured while executing a batch statement.
+					// if result = execute_failed throw error
 					if(updCnt[i]== Statement.EXECUTE_FAILED)
 						throw new DAOException("Fail to insert No"+ i + " SQL");
 				}
 			}
-			//	Coins for JDBC
+		
 		} catch(SQLException se) {
 			//	throw console.log a message error
 			log.error(se.getMessage(),se);
+			//	throw EventException
 			throw new DAOException(new ErrorHandler(se).getMessage());
 			//	handling errors
 		} catch(Exception ex) {
 			//	throw console.log a message error
 			log.error(ex.getMessage(),ex);
+			//	throw EventException
 			throw new DAOException(new ErrorHandler(ex).getMessage());
 		}
 		//	return array
 		return updCnt;
 	}
 	
-	// declare function removeManageErrMsgVOS
+	/**
+	 * [removemanageErrMsgVOS] to remove ErrMsgVO.<br>
+	 * 
+	 * @param ErrMsgVO errMsgVO
+	 * @return int
+	 * @exception DAOException
+	 * @exception Exception
+	 */
 	public int[] removeManageErrMsgVOS(List<ErrMsgVO> errMsgVO) throws DAOException,Exception {
 		//	declare the array delCnt
 		int delCnt[] = null;
 		//	exception handling for arrays
 		try {
-			//	create new SQLExecute
+			// create new SQLExecuter variable to execute query
 			SQLExecuter sqlExe = new SQLExecuter("");
-			//	check the size of the array
+			// check if list not null, execute query to save add new changes in database
 			if(errMsgVO .size() > 0){
-				//	create SQL queries to delete data
+				// execute queries in ErrMsgMngtDBDAOErrMsgVODSQL with param and velParam
 				delCnt = sqlExe.executeBatch((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgVODSQL(), errMsgVO,null);
 				//	iterate the elements in the array
 				for(int i = 0; i < delCnt.length; i++){
-					//	throw an DAOException to know about connection to SQL
+					// EXECUTE_FAILED: The constant indicating that an error occured while executing a batch statement.
+					// if result = execute_failed throw error
 					if(delCnt[i]== Statement.EXECUTE_FAILED)
 						throw new DAOException("Fail to insert No"+ i + " SQL");
 				}
 			}
-			//	Coins for JDBC
+		
 		} catch(SQLException se) {
 			//	throw console.log a message error
 			log.error(se.getMessage(),se);
+			//	throw EventException
 			throw new DAOException(new ErrorHandler(se).getMessage());
 			//	handling errors
 		} catch(Exception ex) {
 			//	throw console.log a message error
 			log.error(ex.getMessage(),ex);
+			//	throw EventException
 			throw new DAOException(new ErrorHandler(ex).getMessage());
 		}
-		//	return array
+	
 		return delCnt;
+	}
+	/**
+	 * [duplicateErrMsgCd] to check duplicate err_msg_cd.<br>
+	 * 
+	 * @param ErrMsgVO errMsgVO
+	 * @return int
+	 * @exception DAOException
+	 * @exception Exception
+	 */
+	
+	public int duplicateErrMsgCd(ErrMsgVO errMsgVO) throws DAOException,Exception{
+		// create and define a null DBRowSet to store query result.
+		DBRowSet dbRowset = null;
+		//query parameter
+		Map<String, Object> param = new HashMap<String, Object>();
+		//velocity parameter
+		Map<String, Object> velParam = new HashMap<String, Object>();
+		int count = 0;
+		try{
+			Map<String, String> mapVO = errMsgVO .getColumnValues();
+
+			param.put("err_msg_cd",errMsgVO.getErrMsgCd());
+
+			// execute queries in ErrMsgMngtDBDAOErrMsgDuplicateRSQL with param and velParam
+			dbRowset = new SQLExecuter("").executeQuery((ISQLTemplate)new ErrMsgMngtDBDAOErrMsgDuplicateRSQL(), param, null);
+			while (dbRowset.next()){
+				String countE = dbRowset.getString(1);
+				count = Integer.parseInt(countE);
+			}
+			System.out.println(count);
+		}
+		catch(SQLException se) {
+			//show error in console with error message
+			log.error(se.getMessage(),se);
+			//throw EventException
+			throw new DAOException(new ErrorHandler(se).getMessage());
+		} catch(Exception ex) {
+			//show error in console with error message
+			log.error(ex.getMessage(),ex);
+			//throw EventException
+			throw new DAOException(new ErrorHandler(ex).getMessage());
+		}
+		return count;
 	}
 	
 }
